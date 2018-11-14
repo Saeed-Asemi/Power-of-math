bbf5e74c2bb7e11dea31398e1ad62e0a4422df6d
Saeed <saeed.asemi1@gmail.com>
2018-11-14 13:04:13 +0330

    Add files via upload

---
+++ Power of math/Power of math/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Power_of_math
+{
+    class Program
+    {
+        static void power(int m , int n)
+        {
+            int a = 1;
+            for(int i=1 ; i<=n ; i++)
+            {
+                a = a * m;
+            }
+            Console.WriteLine($"The result is : {a}");
+        }
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("Please enter number : ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Please enter power of number : ");
+            b = int.Parse(Console.ReadLine());
+            power(a, b);
+            Console.ReadKey();
+        }
+    }
+}
